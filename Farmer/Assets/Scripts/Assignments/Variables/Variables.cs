8bf109914fb4194228f03ecdf8184d2b2924c9e5
Joseph <hatoyo@gmail.com>
2020-01-15 17:17:55 -0700

    Variable lesson 1

--- Farmer/Assets/Scripts/Assignments/Variables/Variables.cs
+++ Farmer/Assets/Scripts/Assignments/Variables/Variables.cs
@@ -4,10 +4,15 @@ using UnityEngine;
 
 public class Variables : MonoBehaviour
 {
-    // Start is called before the first frame update
+    // Declaration
+
+    public float amount;
+    public float money;
+    public float cost;
+    // Start is called before the first frame update, ya silly
     void Start()
     {
-        
+        Cookies(amount, money, cost);
     }
 
     // Update is called once per frame
@@ -15,4 +20,15 @@ public class Variables : MonoBehaviour
     {
         
     }
+
+    void Cookies(float amount, float money, float cost)
+    {
+        float total;
+
+        total = money - cost * amount;
+
+        Debug.Log(total);
+    }
+
+  
 }
