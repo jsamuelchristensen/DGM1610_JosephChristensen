81aab9cc41d9951fc42c3416956c8fbae6f5f7c3
Joey <hatoyo@gmail.com>
2020-01-22 17:14:16 -0700

    DAY one collisions

---
+++ Farmer/Assets/Scripts/Assignments/Variables/Moves.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Moves : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0, 0, 0.3f);
+    }
+
+    // detect collison with another object
+    public void OnCollisionEnter(Collision other)
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        
+    }
+}
