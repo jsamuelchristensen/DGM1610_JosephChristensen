3d0ef459a25f89c538fc1988ab28377d204b29cb
Joey <hatoyo@gmail.com>
2020-01-27 17:13:18 -0700

    Day two collisions/ triggers

--- Farmer/Assets/Scripts/Assignments/Variables/Moves.cs
+++ Farmer/Assets/Scripts/Assignments/Variables/Moves.cs
@@ -13,16 +13,29 @@ public class Moves : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(0, 0, 0.3f);
+        transform.Translate(0, 0, 0.58f);
     }
 
     // detect collison with another object
     public void OnCollisionEnter(Collision other)
     {
-        
+    if (other.gameObject.CompareTag("Floor"))//Primary
+
+    {
+            Debug.Log("Colliding with Floor");
+    }
+    else if (other.gameObject.CompareTag("Obstacle"))//Secondary
+    {
+            Debug.Log("Colliding with Obstacle");
+
+    }
+    else //Default, tertiary
+    {
+            Debug.Log("...");
+    }
     }
     private void OnTriggerEnter(Collider other)
     {
-        
+        Debug.Log("You entered the trigger!");
     }
 }
